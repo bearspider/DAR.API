3971f5420049fe98e60e353781259878e59ea842
Unknown <jrhaddix@gmail.com>
2019-04-13 04:00:41 -0500

    Mostly working

---
+++ DAR.API/Controllers/DARController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DAR.API.Models;
+using DAR.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace DAR.API.Controllers
+{
+    [Route("api/dar")]
+    [ApiController]
+    public class DARController : ControllerBase
+    {
+        private MongoService _mongo;
+        public DARController(MongoService mongoService)
+        {
+            _mongo = mongoService;
+        }
+        // GET: api/dar
+        [HttpGet]
+        public string Get()
+        {
+            return _mongo.GetPackages();
+        }
+        [Route("cleanup")]
+        [HttpGet]
+        public void Clean()
+        {
+            _mongo.Clean();
+        }
+        // GET: api/dar/id
+        [HttpGet("{guid}")]
+        public string Get(string guid)
+        {
+            return _mongo.GetFileName(guid);
+        }
+        // POST: api/dar
+        [Route("payload")]
+        [HttpPost]
+        public void Post(IFormFile file)
+        {
+            _mongo.AddFile(file.OpenReadStream(), file.FileName);
+        }
+        [Route("package")]
+        [HttpPost]
+        public void InsertPackage(Package package)
+        {
+            _mongo.AddPackage(package);
+        }
+        // DELETE: api/dar/id
+        [HttpDelete("{guid}")]
+        public void Delete(String guid)
+        {
+            _mongo.DeletePackage(guid);
+        }
+    }
+}
--- DAR.API/Startup.cs
+++ DAR.API/Startup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DAR.API.Models;
+using DAR.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DAR.API
 {
@@ -27,8 +28,13 @@ namespace DAR.API
         //container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-                opt.UseInMemoryDatabase("TodoList"));
+            //Add MongoDB service
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "DAR API", Version = "v1" });
+            });
+            services.AddScoped<MongoService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -46,7 +52,16 @@ namespace DAR.API
                 // production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DAR API V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseHttpsRedirection();
             app.UseMvc();
         }
